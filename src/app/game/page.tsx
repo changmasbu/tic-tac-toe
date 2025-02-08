d755092571f89a69589d0ab498d61f496f0f5ca0
ChangMabu <hothingoctrang2001@gmail.com>
2025-02-08 21:23:47 +0700

    import image and create music background

---
+++ src/app/game/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+import Game from "@/app/components/Game";
+import React from "react";
+
+export default function GamePage() {
+   return (
+      <div className="flex items-center justify-center min-h-screen bg-[#EEEEEE] text-white">
+         <Game />
+      </div>
+   );
+}

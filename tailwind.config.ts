759e1593919e84f3be9ae841775cd4fa1710a5db
ChangMabu <hothingoctrang2001@gmail.com>
2025-02-08 21:22:58 +0700

    Create layout

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,13 +5,37 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-15deg)' },
+          '50%': { transform: 'rotate(15deg)' },
+        },
+        heartBeat: {
+          '0%': { transform: 'scale(1);' },
+          '14%': { transform: 'scale(1.3);' },
+          '28%': { transform: 'scale(1);' },
+          '42%': { transform: 'scale(1.3);' },
+          '70%': { transform: 'scale(1);' },
+        },
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
+        heartBeat: 'heartBeat 1s infinite',
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        sans: ["var(--font-dm-sans)", "sans-serif"],
+        geist: ["var(--font-geist-sans)", "sans-serif"],
+        mono: ["var(--font-geist-mono)", "monospace"],
+      },
+
     },
   },
   plugins: [],

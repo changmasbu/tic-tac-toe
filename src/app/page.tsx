4705a26b3b48692a4675f2e1c695ce637b7e84d8
ChangMabu <hothingoctrang2001@gmail.com>
2025-02-09 15:00:51 +0700

    Remove import not useuse

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Image from "next/image";
-import Game from "./components/Game";
 import Link from "next/link";
-import "./globals.css"; // Đảm bảo có file chứa CSS
+import "./globals.css";
 
 export default function Home() {
   return (
